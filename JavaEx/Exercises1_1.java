9e88c46a233ba48d0c04968bf76bb30f6d891365
fahryt <111506384+fahryt@users.noreply.github.com>
2022-08-18 04:27:42 -0700

    Add files via upload

---
+++ JavaEx/Exercises1_1.java
@@ -0,0 +1,9 @@
+package JavaEx;
+
+	public class Exercises1_1 {
+		public static void main(String[] args) {
+			System.out.println("Welcome to Java"+
+		"\nWelcome to Computer Science"+"\nProgramming is fun");
+			
+		}
+}
